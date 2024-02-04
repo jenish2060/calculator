a8ceea08a2139d6bb31eaae1ba33d87e5812b349
jenish <114239379+jenish2060@users.noreply.github.com>
2024-02-04 19:31:56 +0545

    Fix file name typo & add basic functions

--- script.js
+++ script.js
@@ -2,19 +2,34 @@ const lastOperation = document.querySelector(".last-operation");
 const currentOperation = document.querySelector(".current-operation");
 const buttonLayout = document.querySelector(".buttons");
 
-function appendLastOperation(value) {
-  lastOperation.append(` ${value}`);
-}
-
-function appendCurrentOperation(value) {
-  currentOperation.textContent = "";
-  if (Number.isInteger(value)) {
-    currentOperation.append(`${value}`);
-  } else {
-    currentOperation.append(`${value.toFixed(3)}`);
-  }
-}
-
 buttonLayout.addEventListener("click", (e) => {
-  console.log(e.target.value);
+  let currentValue = e.target.value;
+  if (currentValue === "clear") {
+    clear();
+  }
 });
+
+function clear() {
+  lastOperation.textContent = "";
+  currentOperation.textContent = "0";
+}
+
+const sum = (firstValue, secondValue) => {
+  return firstValue + secondValue;
+};
+
+const subtract = (firstValue, secondValue) => {
+  return firstValue - secondValue;
+};
+
+const multiply = (firstValue, secondValue) => {
+  return firstValue + secondValue;
+};
+
+const divide = (firstValue, secondValue) => {
+  if (secondValue === 0) {
+    return `ERROR`;
+  } else {
+    return firstValue / secondValue;
+  }
+};
