09e6cbbc92751bd061d52373969d32ea9632ca5f
jenish <114239379+jenish2060@users.noreply.github.com>
2024-02-04 20:33:27 +0545

    Handle result function & update clear function

--- script.js
+++ script.js
@@ -2,16 +2,44 @@ const lastOperation = document.querySelector(".last-operation");
 const currentOperation = document.querySelector(".current-operation");
 const buttonLayout = document.querySelector(".buttons");
 
+let firstValue = null;
+let secondValue = null;
+let operator = null;
+
 buttonLayout.addEventListener("click", (e) => {
   let currentValue = e.target.value;
   if (currentValue === "clear") {
     clear();
+  } else if (currentValue === "delete") {
+    deleteLastOperand;
+  } else if (currentValue === "result") {
+    firstValue = 5;
+    secondValue = 10;
+    operator = "+";
+    if (firstValue != null && secondValue != null) {
+      result();
+    }
+  } else if (typeof parseInt(currentValue) === "number") {
+    handleOperand(parseInt(currentValue));
+  } else {
+    operator = currentValue;
   }
 });
 
 function clear() {
   lastOperation.textContent = "";
   currentOperation.textContent = "0";
+  firstValue = null;
+  secondValue = null;
+  operator = null;
+}
+
+function result() {
+  firstValue = operate(firstValue, secondValue, operator);
+  secondValue = null;
+  operator = null;
+  currentOperation.textContent = "";
+  currentOperation.textContent = `${firstValue}`;
 }
 
 const sum = (firstValue, secondValue) => {
@@ -23,7 +51,7 @@ const subtract = (firstValue, secondValue) => {
 };
 
 const multiply = (firstValue, secondValue) => {
-  return firstValue + secondValue;
+  return firstValue * secondValue;
 };
 
 const divide = (firstValue, secondValue) => {
@@ -33,3 +61,18 @@ const divide = (firstValue, secondValue) => {
     return firstValue / secondValue;
   }
 };
+
+function operate(firstValue, secondValue, operator) {
+  firstValue = parseInt(firstValue);
+  secondValue = parseInt(secondValue);
+  switch (operator) {
+    case "+":
+      return sum(firstValue, secondValue);
+    case "-":
+      return subtract(firstValue, secondValue);
+    case "x":
+      return multiply(firstValue, secondValue);
+    case "÷":
+      return divide(firstValue, secondValue);
+  }
+}
