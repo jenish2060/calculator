cee4a2d7efdbe7cd09d03d159f63b8219ecdff00
jenish <114239379+jenish2060@users.noreply.github.com>
2024-02-04 14:35:32 +0545

    Update id to class for some buttons & get button value

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const lastOperation = document.querySelector(".last-operation");
 const currentOperation = document.querySelector(".current-operation");
-const clear = document.querySelector("#clear");
+const buttonLayout = document.querySelector(".buttons");
+
+function appendLastOperation(value) {
+  lastOperation.append(` ${value}`);
+}
+
+function appendCurrentOperation(value) {
+  currentOperation.textContent = "";
+  if (Number.isInteger(value)) {
+    currentOperation.append(`${value}`);
+  } else {
+    currentOperation.append(`${value.toFixed(3)}`);
+  }
+}
+
+buttonLayout.addEventListener("click", (e) => {
+  console.log(e.target.value);
+});
