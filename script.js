536e1eb34e8f3f8200c2fbc4b976641195caf4f1
jenish <114239379+jenish2060@users.noreply.github.com>
2024-02-05 12:57:13 +0545

    Fix delete button

--- script.js
+++ script.js
@@ -21,9 +21,8 @@ const multiply = (firstValue, secondValue) => {
 
 const divide = (firstValue, secondValue) => {
   if (secondValue == 0) {
-    currentOperation.textContent = `error`;
     setTimeout(clear, 1000);
-    return;
+    return `ERROR`;
   } else {
     return firstValue / secondValue;
   }
@@ -64,9 +63,9 @@ buttonLayout.forEach((button) =>
     if (currentValue === "clear") {
       clear();
     } else if (currentValue === "delete") {
-      deleteLastOperand;
+      deleteLastOperand(currentValue);
     } else if (currentValue === "result") {
-      if (firstValue != null && secondValue != "" && operator != null) {
+      if (firstValue != "" && secondValue != "" && operator != null) {
         result();
       }
     } else if (checkOperator(currentValue)) {
@@ -76,6 +75,24 @@ buttonLayout.forEach((button) =>
     }
   })
 );
+
+function deleteLastOperand(value) {
+  if (secondValue === "") {
+    firstValue = firstValue.substring(0, firstValue.length - 1);
+    currentOperation.textContent = `${firstValue}`;
+    if (firstValue === "") {
+      firstValue = "0";
+      currentOperation.textContent = `${firstValue}`;
+    }
+  } else {
+    secondValue = secondValue.substring(0, secondValue.length - 1);
+    currentOperation.textContent = `${secondValue}`;
+    if (secondValue === "") {
+      secondValue = "0";
+      currentOperation.textContent = `${secondValue}`;
+    }
+  }
+}
 
 function clear() {
   firstValue = "0";
@@ -135,9 +152,10 @@ function updateOperation(value) {
 function result() {
   lastOperation.textContent = `${firstValue} ${operator} ${secondValue} =`;
   firstValue = operate(firstValue, secondValue, operator);
-  if (!Number.isInteger(firstValue)) {
+  if (!Number.isInteger(firstValue) && typeof firstValue !== "string") {
     firstValue = firstValue.toFixed(3);
   }
+  firstValue = String(firstValue);
   secondValue = "";
   operator = null;
   operatorUsedOnce = false;
